c117081eb70a0f66ff450268cf483936583ce824
Duy Tran <tdd310797@gmail.com>
2020-03-28 10:48:31 +0700

    Add Product_Search Page

---
+++ mvc/views/pages/product_search.php
@@ -0,0 +1,195 @@
+<!-- top Products -->
+<div class="ads-grid">
+	<div class="container">
+		<div class="row" style="margin:-50px 0 35px 0">
+			<div class="col-md-3" style="border:1px solid #ebe1e1">
+				<p><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <span style="color:black">SẢN PHẨM CHÍNH HÃNG</span></p>
+				<p style="font-size: 12px">Cam kết về sản phẩm và sức khỏe</p>
+			</div>
+			<div class="col-md-3" style="border:1px solid #ebe1e1">
+				<p><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <span style="color:black">GIÁ THÀNH HỢP LÝ</span></p>
+				<p style="font-size: 12px">Nhập khẩu và Phân phối trực tiếp</p>
+			</div>
+			<div class="col-md-3" style="border:1px solid #ebe1e1">
+				<p><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <span style="color:black">AN TOÀN TÀI CHÍNH</span></p>
+				<p style="font-size: 12px">Giao hàng và thu tiền tận nơi</p>
+			</div>
+			<div class="col-md-3" style="border:1px solid #ebe1e1">
+				<p><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <span style="color:black">TƯ VẤN NHIỆT TÌNH</span></p>
+				<p style="font-size: 12px">Đội ngũ tư vấn có chuyên môn</p>
+			</div>
+		</div>
+		<!-- tittle heading -->
+		<!--<h3 class="tittle-w3l" style="font-size: 30px">Kitchen Products
+			<span class="heading-style">
+				<i></i>
+				<i></i>
+				<i></i>
+			</span>
+		</h3>-->
+		<!-- //tittle heading -->
+		<!-- product left -->
+		<div class="side-bar col-md-3">	
+			<div class="left-side">
+				<h3 class="agileits-sear-head">Danh mục sản phẩm</h3>
+				<ul>
+					<li style="margin-top: 3px; font-size: 15px">
+						<a href="#">Kitchen Products</a>				
+					</li>
+					<li style="margin-top: 3px; font-size: 15px">
+						<a href="#">Motor BCX</a>						
+					</li>		
+					<li style="margin-top: 3px; font-size: 15px">
+						<a href="#">PIN Engal</a>							
+					</li>
+					<li style="margin-top: 3px; font-size: 15px">
+						<a href="#">Escanor CrualSun</a>								
+					</li>
+				</ul>
+			</div>
+			<!-- price range -->
+			<div class="range">
+				<h3 class="agileits-sear-head">Tìm theo giá</h3>
+				<ul class="dropdown-menu6">
+					<li>
+
+						<div id="slider-range"></div>
+						<input type="text" id="amount" style="border: 0; color: #ffffff; font-weight: normal; width:500px" />
+					</li>
+				</ul>
+			</div>
+			<!-- //price range -->											
+			<!-- deals -->
+			<div class="deal-leftmk left-side">
+				<h3 class="agileits-sear-head">Sản phẩm mới</h3>
+				<div class="special-sec1">
+					<div class="col-xs-4 img-deals">
+						<img src="lib/images/d2.jpg" alt="">
+					</div>
+					<div class="col-xs-8 img-deal1">
+						<h3>Lay's Potato Chips</h3>
+						<a href="">$18.00</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="special-sec1">
+					<div class="col-xs-4 img-deals">
+						<img src="lib/images/d1.jpg" alt="">
+					</div>
+					<div class="col-xs-8 img-deal1">
+						<h3>Bingo Mad Angles</h3>
+						<a href="">$9.00</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>				
+				<div class="special-sec1">
+					<div class="col-xs-4 img-deals">
+						<img src="lib/images/d4.jpg" alt="">
+					</div>
+					<div class="col-xs-8 img-deal1">
+						<h3>Tata Salt</h3>
+						<a href="">$15.00</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="special-sec1">
+					<div class="col-xs-4 img-deals">
+						<img src="lib/images/d5.jpg" alt="">
+					</div>
+					<div class="col-xs-8 img-deal1">
+						<h3>Gujarat Dry Fruit</h3>
+						<a href="">$525.00</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="special-sec1">
+					<div class="col-xs-4 img-deals">
+						<img src="lib/images/d3.jpg" alt="">
+					</div>
+					<div class="col-xs-8 img-deal1">
+						<h3>Cadbury Dairy Milk</h3>
+						<a href="">$149.00</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<!-- //deals -->
+		</div>
+		<!-- //product left -->
+		<!-- product right -->
+		<div class="agileinfo-ads-display col-md-9 w3l-rightpro">
+			<div class="wrapper">
+				<!-- first section -->
+				<div class="product-sec1">
+					<div class="col-xs-4 product-men">
+						<div class="men-pro-item simpleCart_shelfItem">
+							<div class="men-thumb-item">
+								<img src="lib/images/k1.jpg" alt="">
+								<div class="men-cart-pro">
+									<div class="inner-men-cart-pro">
+										<a href="single.html" class="link-product-add-cart">Quick View</a>
+									</div>
+								</div>
+								<span class="product-new-top">New</span>
+							</div>
+							<div class="item-info-product ">
+								<h4>
+									<a href="single.html">Zeeba Basmati Rice</a>
+								</h4>
+								<div class="info-product-price">
+									<span class="item_price">$950.00</span>
+									<del>$1020.00</del>
+								</div>
+								<div class="snipcart-details top_brand_home_details item_add single-item hvr-outline-out">
+									<form action="#" method="post">
+										<fieldset>
+											<input type="hidden" name="cmd" value="_cart" />
+											<input type="hidden" name="add" value="1" />
+											<input type="hidden" name="business" value=" " />
+											<input type="hidden" name="item_name" value="Zeeba Basmati Rice - 5 KG" />
+											<input type="hidden" name="amount" value="950.00" />
+											<input type="hidden" name="discount_amount" value="1.00" />
+											<input type="hidden" name="currency_code" value="USD" />
+											<input type="hidden" name="return" value=" " />
+											<input type="hidden" name="cancel_return" value=" " />
+											<input type="submit" name="submit" value="Add to cart" class="button" />
+										</fieldset>
+									</form>
+								</div>
+
+							</div>
+						</div>
+					</div>					
+					<div class="clearfix"></div>
+				</div>
+				<!-- //2nd section  -->				
+				<div style="text-align: center">
+					<ul class="pagination">
+						<li><a href="#">&laquo;</a></li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">&raquo;</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- //product right -->
+	</div>
+</div>
+<!-- //top products -->	
+
+<script>
+	$(document).ready(function () {
+		$("#slider-range").slider({
+			range: true,
+			min: 1000,
+			max: 1000000000,
+			values: [1000, 1000000000],
+			slide: function (event, ui) {
+				$("#amount").val(ui.values[0] + " VND - " + ui.values[1] + " VND");
+			}
+		});
+		$("#amount").val($("#slider-range").slider("values", 0) + " VND - " + $("#slider-range").slider("values", 1) + " VND");
+	});
+</script>
