8a5757bb2307d7ca948b4384b0c34c2a1411e9a5
khoa <dodangkhoagtvt@gmail.com>
2020-03-31 19:24:25 +0700

    push

--- mvc/common/App.php
+++ mvc/common/App.php
@@ -5,16 +5,25 @@
 		protected $params=array();
 		
 		function __construct(){
-			$arr = $this->UrlProcess();
+			$arr = $this->UrlProcess(); $area = "";
+			if(isset($arr[0]) && $arr[0] == "admin")
+			{
+				if(mkdir("/".$arr[0]) == true)
+				{
+					$area = "/".$arr[0];
+					$arr = array_shift($arr);
+					
+				}
+			}
 			if(isset($arr[0]))
 			{
-				if(file_exists("./mvc/controllers/".$arr[0].".php"));
+				if(file_exists(".".$area."/mvc/controllers/".$arr[0].".php"));
 				{
 					$this->controller = $arr[0];
 					unset($arr[0]);
 				}
 			}
-			require_once("./mvc/controllers/".$this->controller.".php");
+			require_once(".".$area."/mvc/controllers/".$this->controller.".php");
 			$temp = $this->controller;
 			$this->controller = new $this->controller;
 			
@@ -34,6 +43,7 @@
 		function UrlProcess(){
 			if(isset($_GET['url']))
 			{
+				echo $_GET['url'];
 				return explode("/",filter_var(trim($_GET['url'],"/")));
 			}
 			return array();
