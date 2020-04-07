df68a47f5eb6989b99582063afecda0472300ebc
Duy Tran <tdd310797@gmail.com>
2020-04-07 23:39:10 +0700

    Fix layout product

--- mvc/views/pages/product.php
+++ mvc/views/pages/product.php
@@ -66,26 +66,30 @@
 							<input name="rangePrice" type="text" id="amount" style="border: 0; color: #ffffff; font-weight: normal; width:500px" />
 							<input id="priceMin" name="priceMin" type="number" value="<?=$data['priceMin']?>" style="display:none" />
 							<input id="priceMax" name="priceMax" type="number" value=<?=$data['priceMax']?> style="display:none" />
-							<button onclick="$('#formfilter').submit()" style="margin-top:10px">Lọc</button>
+							<button onclick="$('#formfilter').submit()" class="btn btn-primary" style="margin-top:15px">Lọc</button>
 						</li>
 					</ul>
 				</div>
 				<!-- //price range -->
 				<!-- food preference -->
 				<div class="left-side">
-					<h3 class="agileits-sear-head">Thương Hiệu</h3>
+					<h3 class="agileits-sear-head">Thương hiệu</h3>
 					<ul>
-						<li style="margin-top: 3px; font-size: 15px">
-							<input type="radio" name="brand" value="0" <?php if($data['brand']==0) echo "checked";?> style="cursor: pointer">
-							<label for="brand"> Tất cả</label>					
+						<li style="margin-top: 3px; font-size: 15px">						
+							<label class="rdo">Tất cả
+								<input type="radio" name="brand" value="0" <?php if($data['brand']==0) echo "checked";?>>
+								<span class="checkmark"></span>
+							</label>
 						</li>
 					<?php
 						foreach($data['listBrands'] as $item)
 						{
 					?>
-						<li style="margin-top: 3px; font-size: 15px">
-							<input type="radio" name="brand" value="<?=$item['BrandId']?>" <?php if($item['BrandId'] == $data['brand']) echo "checked";?> style="cursor: pointer">
-							<label for="brand"> <?=$item['BrandName']?></label>					
+						<li style="margin-top: 3px; font-size: 15px">				
+							<label class="rdo" for="brand"><?=$item['BrandName']?>
+								<input type="radio" name="brand" value="<?=$item['BrandId']?>" <?php if($item['BrandId'] == $data['brand']) echo "checked";?>>
+								<span class="checkmark"></span>
+							</label>
 						</li>
 					<?php
 						}
@@ -103,7 +107,7 @@
 				?>
 					<div class="special-sec1">
 						<div class="col-xs-4 img-deals">
-							<a href="<?=$_SESSION['projectName']?>/Product/Detail/<?=$item['url']?>"><img src="<?=$_SESSION['projectName']?>/lib/images/product/<?=$item['Img']?>" alt=""></a>
+							<a href="<?=$_SESSION['projectName']?>/Product/Detail/<?=$item['url']?>"><img src="<?=$_SESSION['projectName']?>/lib/images/product/<?=$item['Img']?>" alt="err" style="width: 100%; height: 70px"></a>
 						</div>
 						<div class="col-xs-8 img-deal1">
 						<a href="<?=$_SESSION['projectName']?>/Product/Detail/<?=$item['url']?>"><h3><?=$item['ProductName']?></h3></a>
@@ -138,7 +142,7 @@
 						<div class="col-xs-4 product-men">
 							<div class="men-pro-item simpleCart_shelfItem">
 								<div class="men-thumb-item">
-									<img src="<?=$_SESSION['projectName']?>/lib/images/product/<?=$item['Img']?>" alt="">
+									<img src="<?=$_SESSION['projectName']?>/lib/images/product/<?=$item['Img']?>" alt="err" style="width: 100%; height: 200px">
 									<div class="men-cart-pro">
 										<div class="inner-men-cart-pro">
 											<a href="<?=$_SESSION['projectName']?>/Product/Detail/<?=$item['url']?>" class="link-product-add-cart">Xem Chi Tiết</a>
@@ -204,15 +208,15 @@
 		$("#slider-range").slider({
 			range: true,
 			min: 1000,
-			max: 10000000,
-			values: [<?=$data['priceMin']==-1?1000:$data['priceMin']?>, <?=$data['priceMax']==-1?1000000000:$data['priceMax']?>],
+			max: 5000000,
+			values: [<?=$data['priceMin']==-1?1000:$data['priceMin']?>, <?=$data['priceMax']==-1?5000000:$data['priceMax']?>],
 			slide: function (event, ui) {
 				$("#amount").val(ui.values[0] + " VND - " + ui.values[1] + " VND");
 				$("#priceMin").val(ui.values[0]);
 				$("#priceMax").val(ui.values[1]);
 			}
 		});
-		$("#amount").val(<?=$data['priceMin']==-1?1000:$data['priceMin']?> + " VND - " + <?=$data['priceMax']==-1?1000000000:$data['priceMax']?> + " VND");
+		$("#amount").val(<?=$data['priceMin']==-1?1000:$data['priceMin']?> + " VND - " + <?=$data['priceMax']==-1?5000000:$data['priceMax']?> + " VND");
 		
 	});
 	$("input[name='brand']").change(function(){
